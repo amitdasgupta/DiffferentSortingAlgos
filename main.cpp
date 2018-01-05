5264b478191dfeea73e681aefd41d042ff055d40
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-05 20:20:20 +0530

    two arrays sum k in nlogn

--- main.cpp
+++ main.cpp
@@ -4,120 +4,10 @@
 #define pp pair<int,int>
 #include<vector>
 using namespace std;
-class MyComp{
-public:
-    bool operator()(int &a,int &b)
-    {
-        return a>b;
-    }
-};
-/**************bubble sort algorithm
-void bubbleSort(int* &arr,int n)
-{
-    int j=n-2,temp,isSwapp=1;
-    for(;j>=0&&isSwapp;j--)
-    {
-        isSwapp=0;
-        for(int i=0;i<=j;i++)
-        {
-            if(arr[i]>arr[i+1])
-            {
-                isSwapp=1;
-                temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-            }
-        }
-
-    }
-}
-*////////////////
-/**********************selection sort algorithm*
-void selectionSort(int* &arr,int n)
-{
-    int min_,temp,j;
-    for(int i=0;i<n-1;i++)
-    {
-        min_=i;
-        for(j=i;j<n;j++)
-        {
-            if(arr[j]<arr[min_])
-                min_=j;
-        }
-        if(i!=min_)
-        {
-            temp=arr[i];
-            arr[i]=arr[min_];
-            arr[min_]=temp;
-        }
-    }
-}
-///////////////////////////
-/**************************insertion sort algorithm
-void insertionSort(int* &arr,int n)
-{
-    int j,v;
-    for(int i=1;i<n;i++)
-    {
-        j=i;
-        v=arr[i];
-        while(j>0&&arr[j-1]>v)
-        {
-            arr[j]=arr[j-1];
-            j--;
-        }
-        arr[j]=v;
-    }
-}
-*////////////////////////////////////
-/*******************merge sort algorithm
-void mergeIt(int* &arr,int start,int mid,int last)
-{
-    //cout<<"amit";
-    int n1=mid-start,i,j=0;
-    int n2=last-mid+1;
-    int *arr1=new int[n1+1];
-    int *arr2=new int[n2+1];
-    for(i=0;i<n1;i++)
-        arr1[i]=arr[start+i];
-    arr1[n1]=INT_MAX;
-    for(i=0;i<n2;i++)
-        arr2[i]=arr[mid+i];
-    arr2[n2]=INT_MAX;
-    i=0,j=0;
-    for(int k=start;k<=last;k++)
-    {
-        if(arr1[i]<arr2[j])
-        {
-            arr[k]=arr1[i];
-            i++;
-        }
-        else
-        {
-            arr[k]=arr2[j];
-            j++;
-        }
-    }
-    delete[] arr1;
-    delete[] arr2;
-}
-void mergeSort(int* &arr,int start,int last)
-{    if(start>=last)
-        return;
-    else
-    {
-        int mid=(last+start+1)/2;
-        mergeSort(arr,start,mid-1);
-        mergeSort(arr,mid,last);
-        mergeIt(arr,start,mid,last);
-    }
-}*///////////////////////
-
 int partitionForQuickSort(int* &arr,int start,int last)
 {
 
     int pivot=arr[last],temp;
-    //cout<<pivot<<" ";
     int i=start-1;
     for(int k=start;k<last;k++)
     {
@@ -144,42 +34,9 @@ void quickSort(int* &arr,int start,int last)
     {
 
         int pivot=partitionForQuickSort(arr,start,last);
-      //  cout<<pivot<<" ";
         quickSort(arr,start,pivot-1);
         quickSort(arr,pivot+1,last);
     }
-}
-/*************finding repition data in any array*////////////
-void findRepaetingNumberInArray(int *arr,int n)
-{
-    int maxi=INT_MIN,maxiCand,currcount;
-    cout<<"\n";
-    int i=0,temp,flag=0;
-    while(i<n)
-    {
-        currcount=1;
-        flag=0;
-        temp=arr[i];
-        if(temp==arr[i+1])
-        while(temp==arr[i+1])
-        {
-            currcount++;
-            /*if(!flag)
-              {
-                  flag=1;
-                  cout<<temp<<" ";
-              }*/
-            i++;
-        }
-            i++;
-        if(currcount>maxi)
-        {
-            maxi=currcount;
-            maxiCand=temp;
-        }
-
-    }
-    cout<<maxiCand<<" "<<maxi;
 }
 
 /********************************************code for binary search*////////////
@@ -190,7 +47,6 @@ bool isBinary(int *arr,int start,int last,int data)
     else
     {
         int mid=(last+start)/2;
-       // cout<<mid<<endl;
         if(arr[mid]==data)
             return true;
         else
@@ -202,29 +58,7 @@ bool isBinary(int *arr,int start,int last,int data)
 
 
 }
-/*Sort a nearly sorted (or K sorted) array*/
-int* fullySortedArrayFromKSorted(int* arr,int n,int k)
-{
-    vector<int> vec(arr,arr+k+1);
-    make_heap(vec.begin(),vec.begin()+k,MyComp());
-    int i=0,temp,j=k+1,*res;
-    res=new int[n];
-    while(i<n)
-    {
-        temp=*vec.begin();
-       // cout<<*vec.begin()<<" ";
-        pop_heap(vec.begin(),vec.end(),MyComp());
-        cout<<vec.back()<<endl;
-        vec.pop_back();
-        res[i++]=temp;
-        if(j<n)
-        {
-            vec.push_back(arr[j++]);
-        push_heap(vec.begin(),vec.end(),MyComp());
-        }
-    }
-    return res;
-}
+
 void printArray(int *arr,int n)
 {
     for(int i=0;i<n;i++)
@@ -234,15 +68,27 @@ void printArray(int *arr,int n)
 
 int main()
 {
-
-    int n,*arr,i,j,k;
-    cout<<"enter the number of the elements"<<endl;
+/********problem 9*///////////////////
+    int *A,*B,n,temp,flag=0,k;
     cin>>n>>k;
-    arr=new int[n]();
+    A=new int[n];
+    B=new int[n];
     for(int i=0;i<n;i++)
-        cin>>arr[i];
-    int* res=fullySortedArrayFromKSorted(arr,n,k);
-    printArray(res,n);
-    delete[] arr;
+        cin>>A[i];
+    for(int i=0;i<n;i++)
+        cin>>B[i];
+          quickSort(B,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+     temp=k-A[i];
+      if(isBinary(B,0,n-1,temp))
+      {
+          flag=1;
+           break;
+      }
+
+    }
+    if(flag)
+        cout<<"yes";
     return 0;
 }
